39fa58f0132bbe967fd7b8073d27ea40515e4f5e
shoes1085 <91532812+shoes1085@users.noreply.github.com>
2024-06-20 20:50:58 +0800

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,7 @@
-import { useState } from "react";
 import "./styles.css"
-import { NewTodoForm } from "./NewTodoForm";
-import { TodoList } from "./TodoList";
 
 export default function App()
 {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
-
-  function addTodo(title)
-  {
-      setTodos(currentTodos =>{
-        return [
-          ...currentTodos,
-          {
-            id: crypto.randomUUID(),
-            title,
-            completed: false,
-          }
-        ]
-      })
-  }
-
-  function toggleTodo(id, completed)
-  {
-    setTodos(currentTodos =>{
-      return currentTodos.map(todo =>{
-        if (todo.id == id)
-        {
-          return { ...todo, completed}
-        }
-
-        return todo
-      })
-    })
-  }
-
-  function deleteTodo(id)
-  {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
-
-  console.log(todos)
-
   return(
   <>
     <h1>Test</h1>
